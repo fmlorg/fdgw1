31b5dc7c93923ddcf1fca4d4853119b46ef214d6
fukachan <fukachan>
1999-11-22 10:16:40 +0000

    simple dd now :)

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 set -x
-
 dd if=boot.fs of=/dev/rfd0a bs=18k
-
-mount /dev/fd0a /mnt
-test -d /mnt/etc || mkdir /mnt/etc
-cp -pr config/[a-z0-9]*[a-z0-9] /mnt/etc
-umount /mnt
+exit 0;
