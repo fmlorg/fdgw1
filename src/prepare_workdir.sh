48e0333bf635292cabed0e923cfbc7d7a97a89e3
fukachan <fukachan>
2001-12-16 06:15:53 +0000

    check already symlink-ed

--- src/prepare_workdir.sh
+++ src/prepare_workdir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FML: prepare_workdir.sh,v 1.1 2001/12/15 16:29:41 fukachan Exp $
+# $FML: prepare_workdir.sh,v 1.2 2001/12/16 04:04:45 fukachan Exp $
 #
 
 # alloc netbsd source area, which is shared among src.$model
@@ -16,7 +16,9 @@ do
 	for x in ../src/[MN]* ../src/[a-z]*
 	do
 	   if [ -f $x -o -d $x ];then
+		if [ ! -h `basename $x` ];then
 		   ln -s $x .
+		fi
 	   fi
 	done
 done
