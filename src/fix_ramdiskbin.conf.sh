b3deea7eac24d41e08736e60a78e11c7ba709d88
fukachan <fukachan>
2001-12-16 09:19:41 +0000

    oops, define missing else {} condition

--- src/fix_ramdiskbin.conf.sh
+++ src/fix_ramdiskbin.conf.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: fix_ramdiskbin.conf.sh,v 1.1 2001/12/16 09:14:52 fukachan Exp $
 #
 
 cat |(
    if [ ! -d NetBSD/usr.sbin/dhcp/dst ];then
 	echo '# FYI: fix to exclude usr.sbin/dhcp/dst/' 1>&2
 	sed -e 's@ \${BSDSRCDIR}/usr.sbin/dhcp/dst/\*.o@@'
+   else
+	cat
    fi
 )
 
