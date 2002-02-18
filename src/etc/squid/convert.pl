db507a1db5651b2d85646043aecdd7fb8d8a551c
fukachan <fukachan>
2002-02-18 23:28:58 +0000

    icon_directory  /usr/pkg/share/squid/icons error_directory /usr/pkg/share/squid/errors

--- src/etc/squid/convert.pl
+++ src/etc/squid/convert.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# $FML$
+# $FML: convert.pl,v 1.1 2002/02/18 23:22:34 fukachan Exp $
 #
 
 while (<>) {
@@ -15,7 +15,8 @@ while (<>) {
 	s!^\# httpd_accel_uses_host_header.*!httpd_accel_uses_host_header on!;
 	s!^\# httpd_accel_with_proxy.*!httpd_accel_with_proxy on!;
 	s!^\# maximum_object_size.*!maximum_object_size 1 KB!;
-
+	s!^\# icon_directory.*!icon_directory /usr/pkg/share/squid/icons!;
+	s!^\# error_directory.*!error_directory /usr/pkg/share/squid/errors!;
 
 	print $_;
 }
