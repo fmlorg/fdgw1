53522b784af317978d9304f0224c09d3b9e9b3f5
fukachan <fukachan>
2002-04-18 01:54:05 +0000

    check tar.gz by checksum

--- src/extract.sh
+++ src/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: extract.sh,v 1.1 2002/02/21 11:00:47 fukachan Exp $
 #
 
 tmpdir=./trash/$$
@@ -8,24 +8,35 @@ tmpdir=./trash/$$
 cd gnu || exit 1
 
 test -d $tmpdir || mkdir -p $tmpdir
-mv squid* jftpgw* $tmpdir
+mv squid* jftpgw* $tmpdir >/dev/null 2>&1
 
 for tgz in $*
 do
 	name=`basename $tgz .tar.gz`
 	f=../distfiles/$tgz
 
+	echo Extracting $tgz
+
+	checksum0=`grep $tgz distinfo |awk '{print $4}'`
+	checksum1=`digest sha1 $f |awk '{print $4}'`
+	if [ "X$checksum0" = "X$checksum1" ];then
+		echo "Checksum ok"
+	else
+		echo "Error: wrong checksum"
+		exit 1		
+	fi
+
 	echo tar zxf $f
 	eval tar zxf $f
 
 	if expr $name : squid
 	then
-		mv squid* squid
+		mv squid* squid || exit 1
 	fi
 
 	if expr $name : jftpgw
 	then
-		mv jftpgw* jftpgw
+		mv jftpgw* jftpgw || exit 1
 	fi
 done
 
